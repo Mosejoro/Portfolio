cd35a221aa5a92104da3f4da641c131079a292f5
Monehin Oluwaseun John <117038738+Mosejoro@users.noreply.github.com>
2025-03-24 02:27:53 +0100

    Update info.js

--- src/components/info.js
+++ src/components/info.js
@@ -5,13 +5,13 @@ function Info() {
     <div className="info-container" id="infolinker">
       <h2>Our Information</h2>
       <div className="info-details">
-        <div className="info-item">
+        <div className="info-item email">
           <strong>Email:</strong> Monehinseun4125@gmail.com
         </div>
-        <div className="info-item">
+        <div className="info-item phone">
           <strong>Phone:</strong> 08135900015
         </div>
-        <div className="info-item">
+        <div className="info-item address">
           <strong>Address:</strong> GS/135, BIDA, NIGER
         </div>
       </div>
